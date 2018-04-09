93b1cacd431d0aab09b281f425c78eb90d50f939
Paulo Almeida Filho <palmeidaprogramming@gmail.com>
2018-04-09 07:31:03 -0300

    Criado view Cadastro

---
+++ src/com/github/palmeidaprog/financeira/gui/cadastro/ControllerViewCadastro.java
@@ -0,0 +1,37 @@
+package com.github.palmeidaprog.financeira.gui.cadastro;
+
+import com.github.palmeidaprog.financeira.clientes.Bem;
+import com.github.palmeidaprog.financeira.clientes.Cadastro;
+import com.github.palmeidaprog.financeira.clientes.Renda;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TreeTableView;
+
+public class ControllerViewCadastro {
+    private static volatile ControllerViewCadastro instance;
+
+    @FXML private Label credTotalLabel, debNomLabel, bensLabel, credDispLabel;
+    @FXML private Label debTotLabel, rendasLabel;
+    @FXML private Button adicRendaBtn, delRendaBtn, adicBemBtn, delBemBtn;
+    @FXML private Button OkBtn;
+
+    // tables
+    @FXML private TreeTableView<Renda> rendaTable;
+    @FXML private TreeTableColumn<Renda, String> descrRendaCol;
+    @FXML private TreeTableColumn<Renda, String> valorRendaCol;
+    @FXML private TreeTableView<Bem> bemTable;
+    @FXML private TreeTableColumn<Bem, String> descrBemCol;
+    @FXML private TreeTableColumn<Bem, String> valorBemCol;
+
+    // Singleton
+    private ControllerViewCadastro() { }
+
+    public synchronized static ControllerViewCadastro getInstance() {
+        if(instance == null) {
+            instance = new ControllerViewCadastro();
+        }
+        return instance;
+    }
+}
