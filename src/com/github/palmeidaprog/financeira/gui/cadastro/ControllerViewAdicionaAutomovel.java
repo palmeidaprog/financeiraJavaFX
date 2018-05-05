ff9f42bb3fd3c96d24729536afe49cf2c37c2d38
Paulo Almeida Filho <palmeidaprogramming@gmail.com>
2018-05-05 14:32:34 -0300

    [Sync] Salvo

--- src/com/github/palmeidaprog/financeira/gui/cadastro/ControllerViewAdicionaAutomovel.java
+++ src/com/github/palmeidaprog/financeira/gui/cadastro/ControllerViewAdicionaAutomovel.java
@@ -1,19 +1,35 @@
 package com.github.palmeidaprog.financeira.gui.cadastro;
 
+import com.github.palmeidaprog.financeira.clientes.Automovel;
 import com.github.palmeidaprog.financeira.clientes.Cadastro;
+import com.github.palmeidaprog.financeira.clientes.Pendencia;
 import com.github.palmeidaprog.financeira.clientes.Renda;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.util.Pair;
+
+import java.util.HashMap;
 
 public class ControllerViewAdicionaAutomovel {
     private static volatile ControllerViewAdicionaAutomovel instance;
     private Cadastro cadastro;
 
-    @FXML private Label titleLabel;
-    @FXML private TextField descrText, valorText;
-    @FXML private Button adicBtn,cancelBtn;
+    @FXML
+    private Label descrLabel, bemLabel;
+    @FXML
+    private TextField marcaText, modeloText, anoModText, anoFabText,
+            valorText;
+    @FXML
+    private VBox tipoVBox;
+    @FXML
+    private Button adicPendBtn, delPendBtn, criarBtn;
+    @FXML
+    private TableView<Pendencia> pendTable;
+    @FXML
+    private TableColumn<Pendencia, String> descrPendCol;
+    @FXML
+    private TableColumn<Pendencia, double> valorPendCol;
 
     // Singleton
     private ControllerViewAdicionaAutomovel() { }
@@ -30,12 +46,12 @@ public class ControllerViewAdicionaAutomovel {
     }
 
     public void adicBtnClicked() {
-        if(!campoVazio(descrText, "descrição") && !campoVazio(
-                valorText, "valor")) {
+        if(validaCampos()){
             try {
-                double valor = Double.parseDouble(valorText.getText());
-                cadastro.getRendas().inserir(new Renda(valor, descrText
-                        .getText()));
+                cadastro.getBens().inserir(new Automovel(Double.parseDouble(
+                        valorText.getText()), marcaText.getText(), modeloText
+                        .getText(), Integer.parseInt(anoModText.getText()),
+                        Integer.parseInt(anoFabText.getText())));
                 CadastrosViewController.getInstance().fechaAdiciona();
                 ControllerViewCadastro.getInstance().atualizaRendas();
             } catch (NumberFormatException e) {
@@ -47,11 +63,23 @@ public class ControllerViewAdicionaAutomovel {
         }
     }
 
+    private boolean validaCampos() {
+        if(!campoVazio(marcaText, "Marca") &&
+                !campoVazio(modeloText, "Modelo") &&
+                !campoVazio(anoModText, "Ano Modelo") &&
+                !campoVazio(anoFabText, "Ano de Fabricação") &&
+                !campoVazio(valorText, "valor")) {
+            return true;
+        }
+        return false;
+    }
+
+
     private boolean campoVazio(TextField text, String nome) {
         if(text.getText().trim().isEmpty()) {
             CadastrosViewController.getInstance().dialogoErro("Erro",
                     "Você precisa preencher o campo " + nome + " para " +
-                            "adionar a nova renda.");
+                            "adionar o novo automóvel.");
             text.requestFocus();
             return true;
         }
