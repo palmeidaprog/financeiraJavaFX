565721796bfb5e22a428ea7af2ba328a26971223
Paulo Almeida Filho <palmeidaprogramming@gmail.com>
2018-05-16 13:53:35 -0300

    Adicionado bash script com alias para git

---
+++ prjalias.sh
@@ -0,0 +1,2 @@
+alias mergedev="git checkout dev && git merge sync && git checkout sync"
+alias fetchall="git checkout master && git merge origin/master && git checkout dev && git merge origin/dev && git checkout sync && git merge origin/sync"
